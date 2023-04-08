09e4cd3596bbb7726da03bc9dcb1c5e8a1d960ac
Ben Amiel <minebenking@gmail.com>
2023-04-08 12:21:56 +0300

    Mayne removed sql files

--- Database/CompanyTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS companies (
-    company_id INT,
-    company_name VARCHAR(30),
-    manager_id INT,
-    PRIMARY KEY  (company_id),
-    CONSTRAINT FK_manager_id FOREIGN KEY (manager_id)
-    REFERENCES users(id_number)
-)
--- Database/LicensePlateTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS licenseplates (
-    platenumber INT,
-    ownerid INT,
-    PRIMARY KEY (platenumber),
-    CONSTRAINT FK_idnumber FOREIGN KEY (ownerid)
-    REFERENCES users(idnumber)
-
-)
